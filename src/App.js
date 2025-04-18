1545ee97a61ec6561675e8823dc9e7fa8732d015
NurkaAmre <nurkerey30@gmail.com>
2025-04-18 13:52:12 +0200

    Create object

--- src/App.js
+++ src/App.js
@@ -1,22 +1,28 @@
 function App() {
-  return (
-    <div className="steps">
-      <div className="numbers">
-        <div>1</div>
-        <div>2</div>
-        <div>3</div>
-      </div>
-      <p className="message">Hello</p>
-      <div className="buttons">
-        <button style={{ backgroundColor: '#7950f2', color: '#fff' }}>
-          Previous
-        </button>
-        <button style={{ backgroundColor: '#7950f2', color: '#fff' }}>
-          Next
-        </button>
-      </div>
-    </div>
-  );
+
+const messages = [
+  'Learn React ⚛️',
+  'Apply for jobs 💼',
+  'Invest your new income 🤑',
+];
+   return (
+     <div className="steps">
+       <div className="numbers">
+         <div className="active">1</div>
+         <div>2</div>
+         <div>3</div>
+       </div>
+       <p className="message">Hello</p>
+       <div className="buttons">
+         <button style={{ backgroundColor: '#7950f2', color: '#fff' }}>
+           Previous
+         </button>
+         <button style={{ backgroundColor: '#7950f2', color: '#fff' }}>
+           Next
+         </button>
+       </div>
+     </div>
+   );
 }
 
 export default App;
