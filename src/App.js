7f6b5054f3efe09c52f04deb6244fd2d91d2b4e6
NurkaAmre <nurkerey30@gmail.com>
2025-04-19 16:13:22 +0200

    Implement buttons

--- src/App.js
+++ src/App.js
@@ -11,19 +11,27 @@ function App() {
     <div className="steps">
       <div className="numbers">
         <div className={`${step >= 1 ? 'active' : ''}`}>1</div>
-        <div className={`${step >= 1 ? 'active' : ''}`}>2</div>
-        <div className={`${step >= 1 ? 'active' : ''}`}>3</div>
+        <div className={`${step >= 2 ? 'active' : ''}`}>2</div>
+        <div className={`${step >= 3 ? 'active' : ''}`}>3</div>
       </div>
       <p className="message">
-        {messages.map((message) => {
-          message;
-        })}
+        Step {step}: {messages[step - 1]}
       </p>
       <div className="buttons">
-        <button style={{ backgroundColor: '#7950f2', color: '#fff' }}>
+        <button
+          style={{ backgroundColor: '#7950f2', color: '#fff' }}
+          onClick={() => {
+            setStep(step - 1);
+          }}
+        >
           Previous
         </button>
-        <button style={{ backgroundColor: '#7950f2', color: '#fff' }}>
+        <button
+          style={{ backgroundColor: '#7950f2', color: '#fff' }}
+          onClick={() => {
+            setStep(step + 1);
+          }}
+        >
           Next
         </button>
       </div>
