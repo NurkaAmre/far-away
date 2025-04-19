693a16232fb6819c526a0f3bbc5471af708a8d8e
NurkaAmre <nurkerey30@gmail.com>
2025-04-19 16:07:24 +0200

    Apply active button css

--- src/App.js
+++ src/App.js
@@ -1,28 +1,34 @@
-function App() {
-
+import { useState } from 'react';
 const messages = [
   'Learn React ⚛️',
   'Apply for jobs 💼',
   'Invest your new income 🤑',
 ];
-   return (
-     <div className="steps">
-       <div className="numbers">
-         <div className="active">1</div>
-         <div>2</div>
-         <div>3</div>
-       </div>
-       <p className="message">Hello</p>
-       <div className="buttons">
-         <button style={{ backgroundColor: '#7950f2', color: '#fff' }}>
-           Previous
-         </button>
-         <button style={{ backgroundColor: '#7950f2', color: '#fff' }}>
-           Next
-         </button>
-       </div>
-     </div>
-   );
+
+function App() {
+  const [step, setStep] = useState(1);
+  return (
+    <div className="steps">
+      <div className="numbers">
+        <div className={`${step >= 1 ? 'active' : ''}`}>1</div>
+        <div className={`${step >= 1 ? 'active' : ''}`}>2</div>
+        <div className={`${step >= 1 ? 'active' : ''}`}>3</div>
+      </div>
+      <p className="message">
+        {messages.map((message) => {
+          message;
+        })}
+      </p>
+      <div className="buttons">
+        <button style={{ backgroundColor: '#7950f2', color: '#fff' }}>
+          Previous
+        </button>
+        <button style={{ backgroundColor: '#7950f2', color: '#fff' }}>
+          Next
+        </button>
+      </div>
+    </div>
+  );
 }
 
 export default App;
