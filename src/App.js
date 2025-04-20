2507993de4a2cbec1496d3847d70b9fd21da332b
NurkaAmre <nurkerey30@gmail.com>
2025-04-20 17:33:42 +0200

    Create steps file

--- src/App.js
+++ src/App.js
@@ -1,57 +1,9 @@
-import { useState } from 'react';
-
-const messages = [
-  'Learn React ⚛️',
-  'Apply for jobs 💼',
-  'Invest your new income 🤑',
-];
+import Steps from './Steps';
 
 function App() {
-  const [step, setStep] = useState(1);
-  const [toggle, setToggle] = useState(true);
-
-  const HandlePrevious = () => {
-    if (step > 1) {
-      setStep((s) => s - 1);
-    }
-  };
-
-  const HandleNext = () => {
-    if (step < 3) {
-      setStep((s) => s + 1);
-    }
-  };
   return (
     <>
-      <button className="close" onClick={() => setToggle((s) => !s)}>
-        &times;
-      </button>
-      {toggle && (
-        <div className="steps">
-          <div className="numbers">
-            <div className={step >= 1 ? 'active' : ''}>1</div>
-            <div className={step >= 2 ? 'active' : ''}>2</div>
-            <div className={step >= 3 ? 'active' : ''}>3</div>
-          </div>
-          <p className="message">
-            Step {step}: {messages[step - 1]}
-          </p>
-          <div className="buttons">
-            <button
-              style={{ backgroundColor: '#7950f2', color: '#fff' }}
-              onClick={HandlePrevious}
-            >
-              Previous
-            </button>
-            <button
-              style={{ backgroundColor: '#7950f2', color: '#fff' }}
-              onClick={HandleNext}
-            >
-              Next
-            </button>
-          </div>
-        </div>
-      )}
+      <Steps />
     </>
   );
 }
